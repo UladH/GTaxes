291877ab340b30f9c06d98c3654e07ad40cae9e6
UladH <uladzislau.holub@gmail.com>
2023-01-15 19:43:07 +0400

    added general structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'gt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'GTaxes';
+export class AppComponent implements OnInit{
+  //#region constructor
+
+  constructor(
+    private primengConfig: PrimeNGConfig
+  ){    
+  }
+
+  //#endregion
+
+  //#region lifecycle hooks
+
+  public ngOnInit(): void {    
+    this.primengConfig.ripple = true;
+  }
+
+  //#endregion
 }
