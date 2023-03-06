a1cdac66290fe56b6fbe4329b91e405c63ff54c0
UladH <uladzislau.holub@gmail.com>
2023-03-06 10:11:44 +0400

    added currency models

---
+++ src/app/core/models/inner/currency/currency-datestamp.model.ts
@@ -0,0 +1,6 @@
+export class CurrencyDatestampModel{
+    public code!: string;
+    public date!: string;
+    public name!: string;
+    public rate!: number;
+}
---
+++ src/app/core/models/input/currency/currency-datestamp-input.model.ts
@@ -0,0 +1,11 @@
+export class CurrencyDatestampInputModel{
+    public code!: string;
+    public date!: string;
+    public diff!: number;
+    public diffFormated!: string;
+    public name!: string;
+    public quantity!: number;
+    public rate!: number;
+    public rateFormated!: string;
+    public validFromDate!: string;
+}
